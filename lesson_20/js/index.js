e2b0c23aa6c8be2f1b833b80208b97e990113ce4
asechnaya <anutya@mail.ru>
2017-05-16 21:29:58 +0300

    Add files via upload

---
+++ lesson_20/js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'kCd94sCt77E', start: 3 };
+	$('#header1').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
